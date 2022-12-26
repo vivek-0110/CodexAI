e70e2b567aa544b1cf4266fa66bd0a36b4b1ad05
Vivek Kumar Dubey <vivekdubeyvd0110@gmail.com>
2022-12-26 21:13:30 +0530

    updated script.js

--- client/script.js
+++ client/script.js
@@ -86,7 +86,7 @@ const handleSubmit = async (e) => {
     // messageDiv.innerHTML = "..."
     loader(messageDiv)
 
-    const response = await fetch('https://codex-3jpa.onrender.com/', {
+    const response = await fetch('http://codex-3jpa.onrender.com', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
